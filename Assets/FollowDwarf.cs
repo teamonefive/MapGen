c10a61356d6f17728a4cac493bcdeeb0568fc8f7
Jon Volden <volden.jon@gmail.com>
2019-10-17 18:49:46 -0500

    Update dwarf sprite, Added lampy. Follow camera.

---
+++ Assets/FollowDwarf.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowDwarf : MonoBehaviour
+{
+    public GameObject target;
+    private Vector2 pos;
+    public float xOffset = 0f;
+    public float yOffset = 0f;
+
+    void Start() 
+    {
+
+    }
+    void Update()
+    {
+        pos = target.transform.position + new Vector3(xOffset, yOffset, 0f);
+        transform.position = Vector2.MoveTowards(transform.position, pos, 0.1f);
+        Debug.Log(target.transform.position);
+    }
+}
