c272cb66433aacdb3dcb8953b0083e843e0f5ec0
Jon Volden <volden.jon@gmail.com>
2019-11-10 22:34:12 -0600

    Updated sprites. Updated meta files being ignored.

--- Assets/Sprites/dwarf/PlayerMovement.cs
+++ Assets/Sprites/dwarf/PlayerMovement.cs
@@ -5,17 +5,26 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller;
+    public Animator animator;
     public float runSpeed = 40f;
     float horizontalMove = 0f;
     bool jump = false;
 
     void Update()
     {
-        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;        
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        animator.SetFloat("speed", Mathf.Abs(horizontalMove));
+
         if (Input.GetButtonDown("Jump"))
         {
-            jump = true;            
+            jump = true;
+            animator.SetBool("isJumping", true);
         }
+    }
+
+    public void OnLanding()
+    {
+        animator.SetBool("isJumping", false);
     }
 
     void FixedUpdate()
